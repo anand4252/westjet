40ccea586ee7ec89178567abe242a19df4654dfd
Anand R <anand4252@gmail.com>
2022-07-18 21:34:10 -0400

    Refactored session create call

--- profile-management/src/main/java/com/westjet/profilemanagement/SessionClient.java
+++ profile-management/src/main/java/com/westjet/profilemanagement/SessionClient.java
@@ -1,5 +1,6 @@
 package com.westjet.profilemanagement;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.westjet.profilemanagement.builder.MessageHeaderBuilder;
 import com.westjet.profilemanagement.builder.SecurityBuilder;
@@ -7,15 +8,11 @@ import com.westjet.profilemanagement.model.SoapResponse;
 import com.westjet.profilemanagement.model.wsdl.MessageHeader;
 import com.westjet.profilemanagement.model.wsdl.Security;
 import com.westjet.profilemanagement.model.wsdl.SessionCreateRQ;
-import com.westjet.profilemanagement.model.wsdl.SessionCreateRS;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.ws.WebServiceMessage;
-import org.springframework.ws.client.core.WebServiceMessageCallback;
-import org.springframework.ws.client.core.WebServiceMessageExtractor;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.SoapHeader;
 import org.springframework.ws.soap.SoapHeaderElement;
@@ -24,10 +21,8 @@ import org.springframework.ws.soap.saaj.SaajSoapMessage;
 import org.springframework.ws.support.MarshallingUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 
@@ -38,86 +33,61 @@ public class SessionClient extends WebServiceGatewaySupport {
 
     final private SecurityBuilder securityBuilder;
 
-    @SneakyThrows
+
     public SoapResponse createSession() {
 
-//        final Envelope envelope = sessionCreateService.buildEnvelope();
         final SessionCreateRQ sessionCreateRQ = buildSessionCreateRQ();
 
 
-        //Print xml
-//        StringWriter sw = new StringWriter();
-//        JAXB.marshal(envelope, sw);
-//        String xmlString = sw.toString();
-
-//        StringWriter sw = new StringWriter();
-//        Result result = new StreamResult(sw);
-//        this.getMarshaller().marshal(envelope, result);
-//        String xmlString = sw.toString();
-//
-//        System.out.println("xmlString::: ");
-//        System.out.println(xmlString);
-
-
         SoapResponse soapResponse = getWebServiceTemplate().sendAndReceive("https://sws-crt-as.cert.havail.sabre.com/",
-                new WebServiceMessageCallback() {
-                    public void doWithMessage(WebServiceMessage message) throws IOException {
-                        if (message instanceof SaajSoapMessage) {
-                            final SaajSoapMessage saajSoapMessage = (SaajSoapMessage) message;
-                            final SoapHeader soapHeader = saajSoapMessage.getSoapHeader();
+                message -> extracted(sessionCreateRQ, message),
+                this::getSoapResponse);
 
-                            final MessageHeader messageHeader = messageHeaderBuilder.buildMessageHeader();
-                            getWebServiceTemplate().getMarshaller().marshal(messageHeader, soapHeader.getResult());
-                            MarshallingUtils.marshal(getWebServiceTemplate().getMarshaller(), sessionCreateRQ, message);
-
-
-                            final Security security = securityBuilder.buildSecurity();
-                            getWebServiceTemplate().getMarshaller().marshal(security, soapHeader.getResult());
-                            MarshallingUtils.marshal(getWebServiceTemplate().getMarshaller(), sessionCreateRQ, message);
-//                soapHeader.addHeaderElement()
-                        }
-                    }
-                },
-                new WebServiceMessageExtractor<SoapResponse>() {
-                    public SoapResponse extractData(WebServiceMessage message) throws IOException {
-
-                        final SoapMessage soapMessage = (SoapMessage) message;
-                        SoapHeader header = soapMessage.getSoapHeader();
-
-
-                        Iterator<SoapHeaderElement> iterator = header.examineAllHeaderElements();//header.examineHeaderElements(new QName("urn:test", "ResponseHeader"));
-                        final Unmarshaller unmarshaller = getWebServiceTemplate().getUnmarshaller();
-                        Map<String, Object> headers = new HashMap<>();
-
-                        while (iterator.hasNext()) {
-                            final SoapHeaderElement headerElement = iterator.next();
-                            headers.put(headerElement.getName().getLocalPart(),
-                                    unmarshaller.unmarshal(headerElement.getSource()));
-                        }
-
-                        return new SoapResponse(headers, MarshallingUtils.unmarshal(unmarshaller, message));
-                    }
-                });
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        System.out.println("##############################" + objectMapper.writeValueAsString(soapResponse));
-
-//        final Object response = getWebServiceTemplate().marshalSendAndReceive("https://sws-crt-as.cert.havail.sabre.com/", sessionCreateRQ, message -> {
-//            if (message instanceof SaajSoapMessage) {
-//                final SaajSoapMessage saajSoapMessage = (SaajSoapMessage) message;
-//                final SoapHeader soapHeader = saajSoapMessage.getSoapHeader();
-//
-//                final MessageHeader messageHeader = messageHeaderBuilder.buildMessageHeader();
-//                this.getMarshaller().marshal(messageHeader, soapHeader.getResult());
-//
-//                final Security security = securityBuilder.buildSecurity();
-//                this.getMarshaller().marshal(security, soapHeader.getResult());
-////                soapHeader.addHeaderElement()
-//            }
-//        });
+        val objectMapper = new ObjectMapper();
+        try {
+            log.info("Sabre Session successfully created. " + objectMapper.writeValueAsString(soapResponse));
+        } catch (JsonProcessingException jsonProcessingException) {
+            log.error("Error occurred while parsing the Sabre session.");
+        }
 
         return soapResponse;
     }
+
+
+    private void extracted(SessionCreateRQ sessionCreateRQ, WebServiceMessage message) throws IOException {
+        if (message instanceof SaajSoapMessage) {
+            final SaajSoapMessage saajSoapMessage = (SaajSoapMessage) message;
+            final SoapHeader soapHeader = saajSoapMessage.getSoapHeader();
+
+            final MessageHeader messageHeader = messageHeaderBuilder.buildMessageHeader();
+            getWebServiceTemplate().getMarshaller().marshal(messageHeader, soapHeader.getResult());
+            MarshallingUtils.marshal(getWebServiceTemplate().getMarshaller(), sessionCreateRQ, message);
+
+
+            final Security security = securityBuilder.buildSecurity();
+            getWebServiceTemplate().getMarshaller().marshal(security, soapHeader.getResult());
+            MarshallingUtils.marshal(getWebServiceTemplate().getMarshaller(), sessionCreateRQ, message);
+        }
+    }
+
+    private SoapResponse getSoapResponse(WebServiceMessage message) throws IOException {
+        final SoapMessage soapMessage = (SoapMessage) message;
+        SoapHeader header = soapMessage.getSoapHeader();
+
+
+        Iterator<SoapHeaderElement> iterator = header.examineAllHeaderElements();//header.examineHeaderElements(new QName("urn:test", "ResponseHeader"));
+        final Unmarshaller unmarshaller = getWebServiceTemplate().getUnmarshaller();
+        Map<String, Object> headers = new HashMap<>();
+
+        while (iterator.hasNext()) {
+            final SoapHeaderElement headerElement = iterator.next();
+            headers.put(headerElement.getName().getLocalPart(),
+                    unmarshaller.unmarshal(headerElement.getSource()));
+        }
+
+        return new SoapResponse(headers, MarshallingUtils.unmarshal(unmarshaller, message));
+    }
+
 
     private SessionCreateRQ buildSessionCreateRQ() {
         val source = new SessionCreateRQ.POS.Source();
--- profile-management/src/main/java/com/westjet/profilemanagement/controller/ProfileManagementController.java
+++ profile-management/src/main/java/com/westjet/profilemanagement/controller/ProfileManagementController.java
@@ -1,28 +1,31 @@
 package com.westjet.profilemanagement.controller;
 
-import com.example.consumingwebservice.wsdl.GetCountryResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.westjet.core.config.TransformerConfig;
+import com.westjet.core.exception.InvalidInputXmlException;
 import com.westjet.core.helper.CoreHelper;
+import com.westjet.core.model.Node;
+import com.westjet.core.model.TransferDetails;
+import com.westjet.core.service.CoreService;
 import com.westjet.core.service.CoreServiceFactory;
 import com.westjet.profilemanagement.SessionClient;
 import com.westjet.profilemanagement.model.SoapResponse;
-import com.westjet.profilemanagement.model.wsdl.Body;
-import com.westjet.profilemanagement.model.wsdl.Envelope;
-import com.westjet.profilemanagement.model.wsdl.Header;
-import com.westjet.profilemanagement.model.wsdl.MessageHeader;
-import com.westjet.profilemanagement.model.wsdl.ObjectFactory;
 import com.westjet.profilemanagement.model.wsdl.Security;
 import com.westjet.profilemanagement.model.wsdl.SessionCreateRS;
-import com.westjet.profilemanagement.temp.CountryClient;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
 
-import javax.xml.stream.events.EndElement;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
+import java.util.List;
 
 
 @RestController
@@ -34,48 +37,40 @@ public class ProfileManagementController {
     private final TransformerConfig transformerConfig;
     private final CoreHelper coreHelper;
     private final CoreServiceFactory coreServiceFactory;
-
-    private final CountryClient countryClient;
     private final SessionClient sessionClient;
 
     @GetMapping
-    @SneakyThrows
-    public String start() {
+    public String start() throws JsonProcessingException {
         long startTime = System.currentTimeMillis();
-//        System.out.println("Process started!");
-//        final GetCountryResponse response = countryClient.getCountry("Poland");
-//        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ " + response.getCountry().getCurrency());
 
         final SoapResponse soapResponse = sessionClient.createSession();
-        final SessionCreateRS sessionCreateRS = (SessionCreateRS) soapResponse.getBody();
+        final SessionCreateRS responseBody = (SessionCreateRS) soapResponse.getBody();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        final String value = objectMapper.writeValueAsString(sessionCreateRS);
-        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ");
-        System.out.println(value);
+        val objectMapper = new ObjectMapper();
+        log.info("Sabre Create Session body: {}", objectMapper.writeValueAsString(responseBody));
         final Security security = (Security) soapResponse.getHeader().get("Security");
-        System.out.println(security.getBinarySecurityToken());
+
+        log.info("Sabre Session token: {}", security.getBinarySecurityToken());
 
 
-//
-//        try {
-//            String inputXml = coreHelper.getInputXml(transformerConfig.getInputXmlPath());
-//            TransferDetails transferDetails = new TransferDetails(inputXml, List.of());
-//            final List<Node> nodes = transformerConfig.getNodes();
-//            for (Node node : nodes) {
-//                final CoreService coreService = coreServiceFactory.getCoreService(node.getType());
-//
-//                transferDetails = coreService.perform(transferDetails, node.getProperties());
-//                log.info("~~~~result:: " + transferDetails);
-//            }
-//
-//        } catch (InvalidInputXmlException e) {
-//            log.error("Input xml is invalid. Error message: {}", e.getMessage());
-//        } catch (IOException | ParserConfigurationException | TransformerException | SAXException e) {
-//            log.error("Error while applying XSL transformation. Error message: {}", e.getMessage());
-//        } catch (XPathExpressionException e) {
-//            log.error("Error occurred while applying XPath. Error message: {}", e.getMessage());
-//        }
+        try {
+            String inputXml = coreHelper.getInputXml(transformerConfig.getInputXmlPath());
+            TransferDetails transferDetails = new TransferDetails(inputXml, List.of());
+            final List<Node> nodes = transformerConfig.getNodes();
+            for (Node node : nodes) {
+                final CoreService coreService = coreServiceFactory.getCoreService(node.getType());
+
+                transferDetails = coreService.perform(transferDetails, node.getProperties());
+                log.info("~~~~result:: " + transferDetails);
+            }
+
+        } catch (InvalidInputXmlException e) {
+            log.error("Input xml is invalid. Error message: {}", e.getMessage());
+        } catch (IOException | ParserConfigurationException | TransformerException | SAXException e) {
+            log.error("Error while applying XSL transformation. Error message: {}", e.getMessage());
+        } catch (XPathExpressionException e) {
+            log.error("Error occurred while applying XPath. Error message: {}", e.getMessage());
+        }
 
         final long timeTaken = System.currentTimeMillis() - startTime;
         return "Process Completed in ms: " + timeTaken;
